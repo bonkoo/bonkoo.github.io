195287ace0f8ce386d603b8f31d651bdc50414e5
bonkoo <bjkoo91@gmail.com>
2020-09-30 17:59:56 -0500

    Updates

---
+++ precache-manifest.d93b3ab7a689c5cdf5e23d375a5eea6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b67b6f43f75bb92bc96a25f2b463e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daadc0e8e1a11c495736",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d885d3c0d135d606858d",
+    "url": "/static/js/2.9386b27d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9386b27d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daadc0e8e1a11c495736",
+    "url": "/static/js/main.7b6bd8f1.chunk.js"
+  },
+  {
+    "revision": "2d786901f853fbacffe5",
+    "url": "/static/js/runtime-main.38aa7817.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
